bc6cc6f3f73eb4f4b77a57894b8c77f112b2024b
Andrew Azores <aazores@redhat.com>
2024-07-04 13:19:13 -0400

    multiarch

--- gameserver/build.bash
+++ gameserver/build.bash
@@ -10,4 +10,15 @@ function cleanup() {
 }
 trap cleanup EXIT
 
-podman build -t quay.io/redhat-java-monitoring/gameserver-cryostat-agent:latest -f "${DIR}/Containerfile" "${DIR}"
+BUILD_IMG="${APP_REGISTRY:-quay.io}/${APP_NAMESPACE:-redhat-java-monitoring}/${APP_NAME:-gameserver-cryostat-agent}"
+BUILD_TAG="${APP_VERSION:-latest}"
+
+podman manifest create "${BUILD_IMG}:${BUILD_TAG}"
+
+for arch in amd64 arm64; do
+    echo "Building for ${arch} ..."
+    podman build -t "quay.io/redhat-java-monitoring/gameserver-cryostat-agent:linux-${arch}" -f "${DIR}/Containerfile" "${DIR}"
+    podman manifest add "${BUILD_IMG}:${BUILD_TAG}" containers-storage:"${BUILD_IMG}:linux-${arch}"
+done
+
+podman tag "${BUILD_IMG}:${BUILD_TAG}" "${BUILD_IMG}:latest"
