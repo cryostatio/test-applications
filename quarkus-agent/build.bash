3c99fcfe0919e5fd7cc960f235185079dd091907
Andrew Azores <aazores@redhat.com>
2024-07-04 10:11:22 -0400

    test(quarkus): build.bash can be invoked from repo root

--- quarkus-agent/build.bash
+++ quarkus-agent/build.bash
@@ -1,5 +1,15 @@
 #!/usr/bin/env bash
 
-./mvnw -DskipTests clean package
-podman build -t quay.io/redhat-java-monitoring/quarkus-cryostat-agent:latest -f src/main/docker/Dockerfile.jvm .
+set -xe
+
+DIR="$(dirname "$(readlink -f "$0")")"
+
+pushd "${DIR}"
+function cleanup() {
+    popd
+}
+trap cleanup EXIT
+
+"${DIR}/mvnw" -DskipTests clean package
+podman build -t quay.io/redhat-java-monitoring/quarkus-cryostat-agent:latest -f "${DIR}/src/main/docker/Dockerfile.jvm" "${DIR}"
 podman image prune -f
