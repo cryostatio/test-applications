19960f4d70d446681b7bd8654d2e5cc3b9ae9a75
Andrew Azores <aazores@redhat.com>
2024-07-04 13:15:10 -0400

    gradle fixup

--- vertx-fib/build.bash
+++ vertx-fib/build.bash
@@ -5,13 +5,13 @@ set -xe
 DIR="$(dirname "$(readlink -f "$0")")"
 
 BUILD_IMG="${APP_REGISTRY:-quay.io}/${APP_NAMESPACE:-redhat-java-monitoring}/${APP_NAME:-vertx-cryostat-agent}"
-BUILD_TAG="${APP_VERSION:-$(sh "${DIR}/gradlew" -q printVersion)}"
+BUILD_TAG="${APP_VERSION:-$(sh "${DIR}/gradlew" -p "${DIR}" -q printVersion)}"
 
 podman manifest create "${BUILD_IMG}:${BUILD_TAG}"
 
 for arch in amd64 arm64; do
     echo "Building for ${arch} ..."
-    JIB_ARCH="${arch}" sh "${DIR}"/gradlew jibDockerBuild
+    JIB_ARCH="${arch}" sh "${DIR}/gradlew" -p "${DIR}" jibDockerBuild
     podman manifest add "${BUILD_IMG}:${BUILD_TAG}" containers-storage:"${BUILD_IMG}:linux-${arch}"
 done
 
