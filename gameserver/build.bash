243e64e228dc7dbacfc8c5d83fbd69038e9343d7
Andrew Azores <aazores@redhat.com>
2024-07-05 21:49:36 -0400

    test(gameserver): add gameserver test application (#2)

---
+++ gameserver/build.bash
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+
+set -xe
+
+DIR="$(dirname "$(readlink -f "$0")")"
+
+pushd "${DIR}"
+function cleanup() {
+    popd
+}
+trap cleanup EXIT
+
+BUILD_IMG="${APP_REGISTRY:-quay.io}/${APP_NAMESPACE:-redhat-java-monitoring}/${APP_NAME:-gameserver-cryostat-agent}"
+BUILD_TAG="${APP_VERSION:-latest}"
+
+podman manifest create "${BUILD_IMG}:${BUILD_TAG}"
+
+for arch in amd64 arm64; do
+    echo "Building for ${arch} ..."
+    podman build --platform="linux/${arch}" -t "${BUILD_IMG}:linux-${arch}" -f "${DIR}/Containerfile" "${DIR}"
+    podman manifest add "${BUILD_IMG}:${BUILD_TAG}" containers-storage:"${BUILD_IMG}:linux-${arch}"
+done
+
+podman tag "${BUILD_IMG}:${BUILD_TAG}" "${BUILD_IMG}:latest"
