dc13323f7944cf839187dfe1f1f837d875ccc26d
Andrew Azores <aazores@redhat.com>
2024-07-10 12:37:23 -0400

    don't run maven offline

--- quarkus-agent/build.bash
+++ quarkus-agent/build.bash
@@ -11,7 +11,7 @@ function cleanup() {
 trap cleanup EXIT
 
 BUILD_IMG="${APP_REGISTRY:-quay.io}/${APP_NAMESPACE:-redhat-java-monitoring}/${APP_NAME:-quarkus-cryostat-agent}"
-BUILD_TAG="${APP_VERSION:-$(mvn -f "${DIR}/pom.xml" help:evaluate -o -B -q -DforceStdout -Dexpression=project.version)}"
+BUILD_TAG="${APP_VERSION:-$(mvn -f "${DIR}/pom.xml" help:evaluate -B -q -DforceStdout -Dexpression=project.version)}"
 
 "${DIR}/mvnw" -DskipTests clean package
 
