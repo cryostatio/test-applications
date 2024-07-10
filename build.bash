afd982551891e8eaa2456f39831b8e65059d966f
Andrew Azores <aazores@redhat.com>
2024-07-10 14:24:49 -0400

    skip building subdirs without build.bash files (ex. cloned -agent dirs)

--- build.bash
+++ build.bash
@@ -4,8 +4,8 @@ set -xe
 
 for p in */; do
     if [ ! -f "${p}/build.bash" ]; then
-        echo "No build.bash in ${p} !"
-        exit 1
+        echo "No build.bash in ${p} !" 1>&2
+        continue
     fi
     PUSH_MANIFEST="${PUSH_MANIFEST:-false}" TAGS="${TAGS:-latest}" bash "${p}/build.bash"
 done
