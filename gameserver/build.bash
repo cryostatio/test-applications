4aba1b83b969a4c1e18968c7ba5d907f4678ba3a
Andrew Azores <me@andrewazor.es>
2025-01-08 10:15:14 -0500

    chore(gameserver): swap to alternate base image (#28)

--- gameserver/build.bash
+++ gameserver/build.bash
@@ -11,34 +11,26 @@ function cleanup() {
 }
 trap cleanup EXIT
 
+BASE_IMG="${BASE_IMG:-quay.io/webhippie/minecraft-vanilla:latest}"
 BUILD_IMG="${APP_REGISTRY:-quay.io}/${APP_NAMESPACE:-redhat-java-monitoring}/${APP_NAME:-gameserver-cryostat-agent}"
 BUILD_TAG="${APP_VERSION:-latest}"
 CRYOSTAT_AGENT_VERSION="${CRYOSTAT_AGENT_VERSION:-0.5.0-SNAPSHOT}"
 IFS=', ' read -r -a ARCHS <<< "${IMAGE_ARCHS:-amd64 arm64}"
-IFS=', ' read -r -a JDKS <<< "${JDK_VERSIONS:-11 17 21}"
 
-for jdk in "${JDKS[@]}"; do
-    podman manifest create "${BUILD_IMG}:${BUILD_TAG}-jdk${jdk}"
-done
+podman manifest create "${BUILD_IMG}:${BUILD_TAG}"
 
 for arch in "${ARCHS[@]}"; do
-  for jdk in "${JDKS[@]}"; do
-    echo "Building JDK ${jdk} for ${arch} ..."
-    podman build --build-arg server_img="docker.io/itzg/minecraft-server:java${jdk}-jdk" --build-arg agent_version="${CRYOSTAT_AGENT_VERSION,,}" --platform="linux/${arch}" -t "${BUILD_IMG}:linux-${arch}-jdk${jdk}" -f "${DIR}/Containerfile" "${DIR}"
-    podman manifest add "${BUILD_IMG}:${BUILD_TAG}-jdk${jdk}" containers-storage:"${BUILD_IMG}:linux-${arch}-jdk${jdk}"
-  done
+    echo "Building for ${arch} ..."
+    podman build --build-arg server_img="${BASE_IMG}-${arch}" --build-arg agent_version="${CRYOSTAT_AGENT_VERSION,,}" --platform="linux/${arch}" -t "${BUILD_IMG}:linux-${arch}" -f "${DIR}/Containerfile" "${DIR}"
+    podman manifest add "${BUILD_IMG}:${BUILD_TAG}" containers-storage:"${BUILD_IMG}:linux-${arch}"
 done
 
 for tag in ${TAGS}; do
-    for jdk in "${JDKS[@]}"; do
-        podman tag "${BUILD_IMG}:${BUILD_TAG}-jdk${jdk}" "${BUILD_IMG}:${tag}-jdk${jdk}"
-    done
+    podman tag "${BUILD_IMG}:${BUILD_TAG}" "${BUILD_IMG}:${tag}"
 done
 
 if [ "${PUSH_MANIFEST}" = "true" ]; then
     for tag in ${TAGS}; do
-        for jdk in "${JDKS[@]}"; do
-            podman manifest push "${BUILD_IMG}:${tag}-jdk${jdk}" "docker://${BUILD_IMG}:${tag}-jdk${jdk}"
-        done
+        podman manifest push "${BUILD_IMG}:${tag}" "docker://${BUILD_IMG}:${tag}"
     done
 fi
