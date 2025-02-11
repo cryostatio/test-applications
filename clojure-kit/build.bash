190e30f71fb9f963cf879c77bc7890b91f1fb946
Andrew Azores <me@andrewazor.es>
2025-02-11 16:28:34 -0500

    feat(clojure): add clojure-kit example (#33)

---
+++ clojure-kit/build.bash
@@ -0,0 +1,32 @@
+#!/usr/bin/env bash
+
+set -xe
+
+DIR="$(dirname "$(readlink -f "$0")")"
+
+pushd "${DIR}"
+function cleanup() {
+    popd
+}
+trap cleanup EXIT
+
+BUILD_IMG="${APP_REGISTRY:-quay.io}/${APP_NAMESPACE:-redhat-java-monitoring}/${APP_NAME:-clojure-kit}"
+BUILD_TAG="${APP_VERSION:-1.0.0-dev}"
+
+podman manifest create "${BUILD_IMG}:${BUILD_TAG}"
+
+for arch in ${ARCHS:-amd64 arm64}; do
+    echo "Building for ${arch} ..."
+    podman build --pull=missing --platform="linux/${arch}" -t "${BUILD_IMG}:linux-${arch}" -f "${DIR}/Containerfile" "${DIR}"
+    podman manifest add "${BUILD_IMG}:${BUILD_TAG}" containers-storage:"${BUILD_IMG}:linux-${arch}"
+done
+
+for tag in ${TAGS}; do
+    podman tag "${BUILD_IMG}:${BUILD_TAG}" "${BUILD_IMG}:${tag}"
+done
+
+if [ "${PUSH_MANIFEST}" = "true" ]; then
+    for tag in ${TAGS}; do
+        podman manifest push "${BUILD_IMG}:${tag}" "docker://${BUILD_IMG}:${tag}"
+    done
+fi
