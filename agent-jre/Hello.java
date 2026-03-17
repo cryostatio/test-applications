97e485129309e5a5593a49b9e1bd0c4f4e61d0ce
Andrew Azores <me@andrewazor.es>
2026-03-17 10:11:03 -0400

    feat(jre): add Temurin JRE cryostat-agent build

---
+++ agent-jre/Hello.java
@@ -0,0 +1,8 @@
+class Hello {
+    public static void main(String[] args) throws InterruptedException {
+        while (true) {
+            Thread.sleep(1000);
+            System.out.print(". ");
+        }
+    }
+}
